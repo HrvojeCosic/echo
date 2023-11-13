c777e135ba2b1d329e2b83ae3e94bdd2b8653d9a
HrvojeCosic <hrvojecosic333@gmail.com>
2023-11-13 18:14:00 +0100

    CMake: Include tests only when running them

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+WITH_TESTS=$1
+
+if [ -z "$WITH_TESTS" ]; then
+    WITH_TESTS="OFF"
+fi
+
 mkdir -p build
 cd build
-cmake ..
+cmake .. -DWITH_TESTS=$WITH_TESTS
 make
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # (Re)build first
-./scripts/build.sh
+./scripts/build.sh ON
 
 # Find and run all test files
 find build/test -type f -name "*_test" -exec {} \;
